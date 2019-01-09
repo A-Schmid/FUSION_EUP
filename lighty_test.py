fbab9507081776553541a5d25f072b2c127fac8f
Andreas Schmid <Andreas1.Schmid@stud.uni-regensburg.de>
2019-01-09 14:55:54 +0100

    test script for bh1750

---
+++ lighty_test.py
@@ -0,0 +1,13 @@
+import time
+import datetime
+from FUSION import *
+
+sensor = BH1750(42)
+
+while True:
+    light = sensor.getLightIntensity()
+    #log_append_line(filename = "heizung_direkt.txt", message = "{};{};{}".format(dt, temperature, humidity))
+    print(light)
+    time.sleep(5)
+
+time.sleep(5)
