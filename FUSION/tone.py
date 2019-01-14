b673c03ebc572dda20f7362c396dae1f7f91680e
Andreas Schmid <Andreas1.Schmid@stud.uni-regensburg.de>
2019-01-14 15:03:37 +0100

    finalized tone/buzzer, added constants for note frequencies

--- FUSION/tone.py
+++ FUSION/tone.py
@@ -8,6 +8,32 @@ from .packet import *
 INDEX_FREQUENCY = INDEX_DATA
 INDEX_DURATION = INDEX_DATA + 2
 
+NOTE = dict()
+NOTE_BASE = dict()
+NOTES = ['C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'F', 'F#', 'Gb', 'G', 'G#', 'Ab', 'A', 'A#', 'Bb', 'B']
+NOTE_BASE['C'] = 16.35
+NOTE_BASE['C#'] = 17.32
+NOTE_BASE['Db'] = 17.32
+NOTE_BASE['D'] = 18.35
+NOTE_BASE['D#'] = 19.45
+NOTE_BASE['Eb'] = 19.45
+NOTE_BASE['E'] = 20.60
+NOTE_BASE['F'] = 21.83
+NOTE_BASE['F#'] = 23.12
+NOTE_BASE['Gb'] = 23.12
+NOTE_BASE['G'] = 24.50
+NOTE_BASE['G#'] = 25.96
+NOTE_BASE['Ab'] = 25.96
+NOTE_BASE['A'] = 27.50
+NOTE_BASE['A#'] = 29.14
+NOTE_BASE['Bb'] = 29.14
+NOTE_BASE['B'] = 30.87
+
+# calculate note frequencies
+for i in range(0, 7):
+    for c in NOTES:
+        NOTE["{}{}".format(c, i)] = int(NOTE_BASE[c] * (i + 1))
+
 class Tone(Module):
     def __init__(self, node_id):
         Module.__init__(self, node_id)
@@ -24,8 +50,8 @@ class Tone(Module):
             print("sendMessage error")
 
     def playTone(self, frequency, duration):
-        # TODO
-        self.sendMessage([0xF0, 0x00, 0x00, 0xFF, 0x00, 0x00])
+        message = struct.pack(">HI", frequency, duration)
+        self.sendMessage(list(message))
 
     def stopTone(self):
         self.sendMessage([0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
--- buzzertest.py
+++ buzzertest.py
@@ -3,7 +3,11 @@ from FUSION import *
 t = Tone(40)
 
 while(True):
-    t.playTone(0, 0)
+    t.playTone(NOTE["A2"], 1000)
+    time.sleep(1)
+    t.playTone(NOTE["C#2"], 1000)
+    time.sleep(1)
+    t.playTone(NOTE["E2"], 1000)
     time.sleep(1)
     t.stopTone()
     time.sleep(1)
