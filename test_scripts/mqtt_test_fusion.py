76c4794d8be720c211430cfd5206609a4a9bc635
Andreas Schmid <Andreas1.Schmid@stud.uni-regensburg.de>
2019-01-28 17:43:06 +0100

    WIP two way MQTT 2

--- test_scripts/mqtt_test_fusion.py
+++ test_scripts/mqtt_test_fusion.py
@@ -8,12 +8,10 @@ sensor = DHT11_MQTT.DHT11_MQTT(node_name = "tempi", node_location="1104")
 def on_temp():
     print("{} Grad Celsius".format(sensor.get("temperature")))
 
+def on_humi():
+    print("{} Prozent Luftfeuchtigkeit".format(sensor.get("humidity")))
+
 sensor.OnUpdate(on_temp, "temperature")
-
-print("go!")
-
-#for i in range(1, 100):
-#    print("loop {}".format(sensor.get("temperature")))
-#    time.sleep(1)
+sensor.OnUpdate(on_humi, "humidity")
 
 time.sleep(60)
