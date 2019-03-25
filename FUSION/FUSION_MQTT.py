00d7e7c376e79fe364abd65334e73a262a20b05d
Andreas Schmid <schmid@schmid.uni-regensburg.de>
2019-03-25 15:14:12 +0100

    wrote comments for FUION_MQTT

--- FUSION/FUSION_MQTT.py
+++ FUSION/FUSION_MQTT.py
@@ -2,22 +2,33 @@ from .core import *
 import struct
 import paho.mqtt.client as mqtt
 
+# main class for MQTT based nodes
+# extend this one if you want to write your own nodes
+# handles connection and communication, parses and stores data, calls callback functions
 class FUSION_MQTT():
     def __init__(self, node_name, node_network=MQTT_DEFAULT_NETWORK, node_location=MQTT_DEFAULT_LOCATION):
         self.__last_update = 0
 
+        # data is structured based on key/value pairs with an arbitrary "data entry" string as a key
+        # each type of sensor data (like "temperature", "humidity", ...) can be used as a data entry key
+        # sensor nodes know which type of data they publish (it's defined in the library)
+        # so similar sensor nodes are exchangeable and you only have to make the model of your node listen to the correct data entry by adding the entry to the following list
         self.data_entries = []
 
+        # keeps track of all registered callbacks - data entry specific callbacks are stored under the corresponding key
         self._callbacks = dict()
         self._callbacks["all"] = []
 
+        # contains actual sensor data with data entry strings as keys. gets updated each time new data arrives
         self.data = dict()
         self._data_types = dict()
 
+        # the MQTT topic to listen to is defined here
         self.mqtt_network = node_network
         self.mqtt_location = node_location
         self.mqtt_name = node_name
 
+        # register MQTT callbacks
         self._mqtt = mqtt.Client()
         self._mqtt.on_connect = self.on_connect
         self._mqtt.on_message = self.on_message
@@ -30,6 +41,9 @@ class FUSION_MQTT():
 
         print("loop started")
 
+    # use this function to add new data entries to the object
+    # data_entry (String) is a string representing the type of data the sensor measures (like "temperature")
+    # data_type (Type) is the data type of the sensor data. it is sometimes necessary to specify it so the data can be deserialized correctly
     def add_data_entry(self, data_entry, data_type=None):
         print("data entry: " + data_entry)
         self.data_entries.append(data_entry)
@@ -40,22 +54,27 @@ class FUSION_MQTT():
     def on_connect(self, client, userdata, flags, result_code):
         #print("on_connect: " + result_code)
         #self._mqtt.subscribe("{}/{}/{}/#".format(self.mqtt_network, self.mqtt_location, self.mqtt_name))
+        # we listen to the specified topic defined by NETWORK, LOCATION and NODE_NAME with a wildcard for the data entry
         self._mqtt.subscribe("{}/{}/{}/#".format(self.mqtt_network, self.mqtt_location, self.mqtt_name))
 
+    # checks if the received message matches a data entry our object listens to
+    # if this is the case, decode the message, store the data and call corresponding callbacks
     def on_message(self, client, userdata, msg):
         for entry in self.data_entries:
             if(msg.topic.split("/")[-1] == entry):
                 self.data[entry] = self._decode_message(msg.payload, self._data_types[entry]) 
                 for callback in self._callbacks[entry]:
-                    callback()
+                    callback(self.data[entry])
         for callback in self._callbacks["all"]:
             callback()
 
+    # send a message to the specified node
     def send_message(self, topic, payload=None, qos=0, retain=False):
         theTopic = "{}/{}/{}/{}".format(self.mqtt_network, self.mqtt_location, self.mqtt_name, topic)
         #print(theTopic, payload)
         self._mqtt.publish(theTopic, payload, qos, retain)
 
+    # deserialize the binary data from a message to a matching data type
     def _decode_message(self, data, data_type):
         if(data_type == bool):
             return struct.unpack("<?", data)[0]
@@ -76,9 +95,12 @@ class FUSION_MQTT():
         elif(data_type == str):
             return data.decode("UTF-8")
 
+    # get the last received data for data_entry
     def get(self, data_entry):
         return self.data[data_entry]
 
+    # registers a callback to a specific data entry
+    # this callback is called whenever data for this entry is updated
     def OnUpdate(self, callback, data_entry=None):
         if(data_entry == None):
             self._callbacks["all"].append(callback)
