65657d351e2e09aacd97c9f19d63f78e73150dd8
Andreas Schmid <andreas1.schmid@schmid.uni-regensburg.de>
2019-04-24 15:20:13 +0200

    before pre study

--- FUSION/email.py
+++ FUSION/email.py
@@ -5,8 +5,10 @@ def send_email(recipient, subject="FUSION", message=""):
     msg = EmailMessage()
     msg.set_content(message)
     msg['Subject'] = subject
-    msg['From'] = "fusion@fusion.de"
+    msg['From'] = "Andreas1.Schmid@stud.uni-regensburg.de"
     msg['To'] = recipient
-    server = smtplib.SMTP_SSL("mail.uni-regensburg.de", 465)
+    server = smtplib.SMTP("smtp.uni-regensburg.de", 587)
+    server.starttls()
+    server.login("sca04209", "password")
     server.send_message(msg)
     server.quit()
